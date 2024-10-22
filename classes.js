19744d73a6c5deb23c27a952ea9f1a8047fe3be8
sunny <ethsok@protonmail.com>
2024-10-22 12:44:58 -0400

    Net ninjas OOP javascript course

--- classes.js
+++ classes.js
@@ -2,14 +2,31 @@ class User {
     constructor(email, name){
         this.email = email
         this.name = name
+        this.score = 0
     }
 
     login(){
         console.log(this.email, 'just logged in')
+        return this
     }
 
     logout(){
         console.log(this.email, 'just logged out')
+        return this
+    }
+    updateScore(){
+        this.score++
+        console.log(this.email, 'score is now', this.score)
+        return this
+    }
+}
+
+class Admin extends User{
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email != user.email;
+        })
+
     }
 
 }
@@ -18,8 +35,13 @@ let userOne = new User('ryu@gmail.com', 'Ryu')
 
 let userTwo = new User('yoshi@mario.com', 'Yoshi')
 
+let admin = new Admin('bob@gmail.com','shaun')
 
-console.log(userOne,userTwo)
+let users = [userOne, userTwo]
 
-userTwo.login()
-userOne.logout()
+console.log(users)
+
+admin.deleteUser(userOne)
+
+console.log(users)
+
